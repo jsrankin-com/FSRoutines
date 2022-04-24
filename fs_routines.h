15009cb89ca29f41234fe16d86b49f28a44c5819
jsrankin-com <89367845+jsrankin-com@users.noreply.github.com>
2022-04-23 23:53:15 -0400

    Update fs_routines.h
    merge_files(), readable_size() added.

--- fs_routines.h
+++ fs_routines.h
@@ -1,5 +1,12 @@
 #pragma once
 #include <iostream>
 #include <filesystem>
+#include <sstream>
+#include <fstream>
+#include <vector>
 
 void copy_file(std::filesystem::path, std::filesystem::path);
+
+void merge_files(std::vector<std::filesystem::path>);
+
+std::string readable_size(std::filesystem::path);
